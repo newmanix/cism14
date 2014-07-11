1aac029f5e404351c3fcee9b8623b611be938b65
Bill Newman <william.newman@seattlecolleges.edu>
2014-07-10 18:20:17 -0700

    Adds initial customer model

--- application/models/customers_model.php
+++ application/models/customers_model.php
@@ -8,9 +8,14 @@ class Customers_model extends CI_Model {
 		
 	}
 	
-	public function get_customers()
+	public function get_customers($id = 0)
 	{
-		$query = $this->db->get('test_Customers');
+		if((int)$id == 0)
+		{//id  is zero, show all!
+			$query = $this->db->get('test_Customers');
+		}else{//show one customer
+			$query = $this->db->get_where('test_Customers',array('CustomerID'=>$id));
+		}
 		return $query->result_array();
 	
 	}
